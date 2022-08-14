2c850dcfaf953c73512284647fb30582c7bd0af2
ogenkidesu <89525255+ogenkidesu@users.noreply.github.com>
2022-08-14 10:38:33 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ global.packname = 'vams=md' //Ganti Jadi Nama Botmu
 global.author = 'By mangtoi' //Ganti Jadi Namamu
 global.prefa = ['','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'session' //Saran Gausah Diganti Lagi
-global.webme = 'https://linktr.ee/NzrlAfndi' //Ganti Jadi Bebas Web Sosmed Mu
+global.webme = 'https://linktr.ee/vams_store' //Ganti Jadi Bebas Web Sosmed Mu
 
 //====> SARAN GAUSAH DIGANTI SIH DAH PAS SEMUA
 global.limitAwal = {
